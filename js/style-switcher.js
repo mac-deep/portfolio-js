f25ff6c4b1da56835e878f4abd9b28cf9eb224e9
Deep Makadiya <deepmac.25@gmail.com>
2021-01-08 19:40:57 +0530

    responsiveness added

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -11,9 +11,15 @@ window.addEventListener("scroll", () => {
 });
 
 const alternateStyle = document.querySelectorAll(".alternate-style");
+
 function setActivityStyle(color) {
+  localStorage.setItem("color", color);
+  changeColor();
+}
+
+function changeColor() {
   alternateStyle.forEach((style) => {
-    if (color === style.getAttribute("title")) {
+    if (localStorage.getItem("color") === style.getAttribute("title")) {
       style.removeAttribute("disabled");
     } else {
       style.setAttribute("disabled", "true");
@@ -21,20 +27,42 @@ function setActivityStyle(color) {
   });
 }
 
+if (localStorage.getItem("color") !== null) {
+  changeColor();
+}
+
 // theme chnager
 
 const dayNight = document.querySelector(".day-night");
 
 dayNight.addEventListener("click", () => {
-  dayNight.querySelector("i").classList.toggle("fa-sun");
-  dayNight.querySelector("i").classList.toggle("fa-moon");
   document.body.classList.toggle("dark");
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+  updateIcon();
 });
 
-window.addEventListener("load", () => {
+function themeMode() {
+  if (localStorage.getItem("theme") !== null) {
+    if (localStorage.getItem("theme") === "light") {
+      document.body.classList.remove("dark");
+    } else {
+      document.body.classList.add("dark");
+    }
+  }
+  updateIcon();
+}
+themeMode();
+
+function updateIcon() {
   if (document.body.classList.contains("dark")) {
+    dayNight.querySelector("i").classList.remove("fa-moon");
     dayNight.querySelector("i").classList.add("fa-sun");
   } else {
+    dayNight.querySelector("i").classList.remove("fa-sun");
     dayNight.querySelector("i").classList.add("fa-moon");
   }
-});
+}
